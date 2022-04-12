13ccbb86e42fb819838d016e02abbfe076183d3a
sumendra-manandhar <thesume.2010@gmail.com>
2022-04-12 15:39:58 +0545

    recent works added

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,8 +29,23 @@ tabs.forEach((tab) => {
 });
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
-
+let mixerPortfolio = mixitup(".work__container", {
+  selectors: {
+    target: ".work__card",
+  },
+  animation: {
+    duration: 300,
+  },
+});
 /*===== Link Active Work =====*/
+const linkWork = document.querySelectorAll(".work__item");
+
+function activeWork() {
+  linkWork.forEach((l) => l.classList.remove("active-work"));
+  this.classList.add("active-work");
+}
+
+linkWork.forEach((l) => l.addEventListener("click", activeWork));
 
 /*===== Work Popup =====*/
 
